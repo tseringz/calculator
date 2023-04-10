71a004f4861dc607eb91d29e8b2d0e979f431e08
tseringz <tserockon77@gmail.com>
2023-04-10 10:40:22 +0530

    Refactor the Code

--- script.js
+++ script.js
@@ -1,169 +1,105 @@
-// build my on fuction to evaluate expression 
-function add (a, b) {
-    return  a + b;
+
+let a  = null;
+let b = null;
+let result;
+let screenDisplay = '';
+let operator = '';
+let deletedNumber;
+
+const numberBtns = document.querySelectorAll('#number');
+const screen = document.getElementById('screen');
+const operateSigns = document.querySelectorAll('#operator');
+const clearBtn = document.querySelector('#button-clear');
+const deleteBtn = document.querySelector('#button-delete');
+
+
+function addNumber() {
+    return a + b;
 }
 
-function subtract(a, b) {
+function subtractNumber() {
     return a - b;
 }
 
-function multiply(a, b) {
-    return  a * b;
+function divideNumber() {
+    return (b === 0) ? "Your math sucks!" : a / b;
 }
 
-function divide(a, b) {
-    if ( b === 0 ) {
-        return "Hi Dump!"; 
-    }
-
-    let num = a / b;
-    if (Number.isInteger(num)) {
-        return num;
-    }
-
-    else {
-    return num.toFixed(3);
- }
-    
-}
-
-function operate(operator, a, b) {
-
-    if(operator === "+") {
-        return add(a, b);
-    }
-    else if(operator === "-") {
-       return  subtract(a, b);
-    }
-    else if(operator === "*") {
-
-       return  multiply(a, b);
-
-    }
-    else if (operator === "/") {
-
-       return  divide(a, b);
-
-    }
-    else if (operator === "=") {
-
-        return a;
-    }
+function multiplyNumber() {
+    return a * b;
 }
 
 
-function displayOnScreen() {
+function UpdateOperate(operator) {
+    if (operator === '+') {
+        return addNumber();
+    } else if (operator === '-') {
+        return subtractNumber();
+    } else if (operator === '*') {
+        return multiplyNumber();
+    } else if (operator === '/') {
+        return divideNumber();
+    }
+}
 
-    let firstNumber = [];
-    let secondNumber = [];
-    let result = 0;
-    let operator = "";
-    let counter = 0;
-    let dotCounter = 0;
-    // selecting every html tag that will use on the calculator function
-    const screen = document.querySelector('.screen');
-    const numberButtons = document.querySelectorAll('.number-wrapper > button');
-    screen.innerText = "0";
-    const buttonClear = document.querySelector('#button-clear');
-    const buttonDelete = document.querySelector('#button-delete');
+// **** Clear the screen **** 
+function clearScreen() {
+   a  = null;
+   b = null;
+   result;
+   screenDisplay = '';
+   operator = '';
+   operatorTwo = '';
+   screen.innerText = '0';
+}
+clearBtn.addEventListener('click', clearScreen);
 
-    // function to clear the screen 
-    buttonClear.addEventListener('click', () => {
-        firstNumber = [];
-        secondNumber = [];
-        result = 0;
-        operator = "";
-        counter = 0;
-        screen.innerText = "0";
+// **** Delete numbers **** 
+function deleteScreen() {  
+        screenDisplay = screenDisplay === '' ? result.toString() : screenDisplay;
+        screenDisplay = screenDisplay.slice(0, screenDisplay.length - 1);
+        screen.innerText = (screenDisplay !== '') ? screenDisplay : 0;
+}
+
+deleteBtn.addEventListener('click', deleteScreen);
+
+
+function displayScreen(displayValue) {
+    screenDisplay = screenDisplay + displayValue;
+    screen.innerText = screenDisplay;
+}
+
+
+numberBtns.forEach(numberBtn => {
+    numberBtn.addEventListener('click', (e) => {
+    displayScreen(e.target.value.toString());
+    console.log(e.target.value);
     });
+});
 
 
-    // walk through each butttons on the calculator accept clear and delete button
-    numberButtons.forEach( element => {
-         element.addEventListener('click', (e) => {
-            if (e.target.value !== '+' && e.target.value !== '-' && e.target.value !== '=' &&
-              e.target.value !== "*" && e.target.value !== '/') {
-                 if (counter <= 0) {
-
-                  firstNumber.push(e.target.value);
-                  if(e.target.value === ".") {
-                      dotCounter++;
-                        if(dotCounter > 1)
-                        {
-                            firstNumber.pop();
-                        }
-                  }
-                         screen.innerText = firstNumber.join("");
-                   }
-
-                  if(counter >= 1) {
-                   secondNumber.push(e.target.value);
-                   screen.innerText = +secondNumber.join("");
-                    }
-
-        }
-
-        if (e.target.value === "+" || e.target.value === "-" || e.target.value === "/" 
-            || e.target.value === "=" || e.target.value === "*") {
-
-                if (counter <= 0) {
-                    if (e.target.value === '*' || e.target.value === '/') {
-                        operator = e.target.value; 
-                        secondNumber = [1];
-                        result =  operate(operator, +firstNumber.join(""), +secondNumber.join(""));
-                        screen.innerText = result; 
-                        counter++;
-                    }
-                
-                    else if (e.target.value === "=") {
-                        result = 0;
-                    } 
-
-                    else {
-                        operator = e.target.value; 
-                        result =  operate(operator, +firstNumber.join(""), +secondNumber.join(""));
-                        screen.innerText = result; 
-                        counter++;
-                    }
-                }
-                
-                if (counter >= 1) {
-                        firstNumber = [result];
-                        result =  operate(operator, +firstNumber.join(""), +secondNumber.join(""));
-                        screen.innerText = result; 
-                        operator = e.target.value;
-                        secondNumber = [];
-               }
-         }
-           
-
-       });
+operateSigns.forEach(operateSign => {
+    operateSign.addEventListener('click', (e) => {
+        if (a === null) {
+            b = (e.target.value === '*' || e.target.value === '/') ? 1 : 0; // Avoid errors when multiply and divide by zero
+            a = Number(screenDisplay);
+            operator = e.target.value; 
+            result = UpdateOperate(operator);
+            screen.innerText = result.toString();
+            screenDisplay = '';    
             
-   });
-
-   // Function to delete the numbers when they click the delete button
-    buttonDelete.addEventListener('click', (e) => {
-    const numberDelete =  screen.innerText.split('');
-
-                          if (numberDelete.length > 1) {
-                            numberDelete.pop();
-                            screen.innerText = numberDelete.join("");
-                            firstNumber = numberDelete;
-                            result = numberDelete.join("");
-
-                          } else {
-                            firstNumber = [];
-                            secondNumber = [];
-                            result = 0;
-                            operator = "";
-                            counter = 0;
-                            screen.innerText = '0';
-                          }
-                          
-   });
-
-}
-displayOnScreen();
+        } else {
+            a = result;
+            b = Number(screenDisplay);
+            result = UpdateOperate(operator);
+            screen.innerText = result.toString();
+            operator = e.target.value; 
+            b = '';
+            screenDisplay = '';
+        }
+        
+    })
+});
 
 
 function updateClocks() {
@@ -182,3 +118,4 @@ function updateClocks() {
 // Update every minute:
 setInterval(updateClocks, 60000);
 updateClocks();
+
